908d7cd68371d9920b6840245b05569f930a158f
dev0-arslan <arslanansarii947@gmail.com>
2022-08-27 11:25:51 -0700

    Program No. 01

---
+++ app.ts
@@ -0,0 +1,17 @@
+// Program No. 01
+// 1. Write a ts program to find maximum between two numbers.
+// Enter Tow Numbers:
+
+var num1:number = 10;
+var num2:number = 20;
+
+if (num1 > num2) {
+    console.log("num1 is greater than num2");
+}
+else if (num2 > num1) {
+    console.log("num2 is greater than num1");
+}
+else{
+    console.log("num1 is equal to num2");
+    
+}
