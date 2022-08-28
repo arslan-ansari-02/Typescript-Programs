ec4f84f6791ba81b668cd28dfc8a696386d8ac99
dev0-arslan <arslanansarii947@gmail.com>
2022-08-28 12:34:52 -0700

    Add Program N0.6

--- app.js
+++ app.js
@@ -1,69 +1,76 @@
 // Program No. 01
 // 1. Write a ts program to find maximum between two numbers.
 // Enter Tow Numbers:
-var num1 = 10;
-var num2 = 20;
-if (num1 > num2) {
-    console.log("num1 is greater than num2");
-}
-else if (num1 < num2) {
-    console.log("num2 is greater than num1");
-}
-else {
-    console.log("both numbers are equal");
-}
+// var num1:number = 10;
+// var num2:number = 20;
+// if (num1 > num2) {
+//     console.log("num1 is greater than num2");
+// }
+// else if (num1 < num2) {
+//     console.log("num2 is greater than num1");
+// }
+// else{
+//     console.log("both numbers are equal");
+// }
 // Program No. 02
 // 2. Write a ts program to find maximum between Three numbers.
 // Enter Three Numbers:
-var num3 = 10;
-var num4 = 20;
-var num5 = 30;
-if (num3 > num4 && num3 > num5) {
-    console.log("num3 is greater both num4 and num5");
-}
-else if (num4 > num3 && num5 > num5) {
-    console.log("num4 is greater both num3 and num5");
-}
-else if (num5 > num3 && num5 > num2) {
-    console.log("num5 is greater both num3 and num4");
-}
-else {
-    console.log("All three numbers are equal");
-}
+// var num3:number = 10
+// var num4:number = 20
+// var num5:number = 30
+// if (num3>num4 && num3>num5) {
+//     console.log("num3 is greater both num4 and num5");
+// }
+// else if (num4>num3 && num5>num5){
+//     console.log("num4 is greater both num3 and num5");
+// }
+// else if (num5>num3 && num5>num2){
+//     console.log("num5 is greater both num3 and num4");
+// }
+// else{
+//     console.log("All three numbers are equal");
+// }
 // Program No. 03
 // 3. Write a ts program to check whether a number is negative, positive or zero.
-var num6 = 1;
-if (num6 < 0) {
-    console.log("num6 is Negative");
-}
-else if (num6 > 0) {
-    console.log("num6 is Positive");
-}
-else {
-    console.log("num6 is zero");
-}
+// var num6:number = 1
+// if (num6 < 0) {
+//     console.log("num6 is Negative");
+// }
+// else if (num6 > 0) {
+//     console.log("num6 is Positive");
+// }
+// else{
+//     console.log("num6 is zero");
+// }
 // Program Node. 04
 // 4. Write a ts program to check whether a number is divisible by 5 and 11 or not.
-var numSet = 110;
-if (numSet % 5 == 0 && numSet % 11 == 0) {
-    console.log("the enter number is divisible by 5 and 11", numSet);
-}
-else if (numSet % 5 == 0) {
-    console.log("this number only divisible on 5");
-}
-else if (numSet % 11 == 0) {
-    console.log("this number only divisible on 11");
-}
-else {
-    console.log("this number is not divisibl on them");
-}
+// var numSet = 110;
+// if (numSet % 5 == 0 && numSet % 11 == 0) {
+//     console.log("the enter number is divisible by 5 and 11", numSet);
+// }
+// else if (numSet % 5 == 0) {
+//     console.log("this number only divisible on 5");
+// }
+// else if (numSet % 11 == 0) {
+//     console.log("this number only divisible on 11");
+// }
+// else {
+//     console.log("this number is not divisibl on them");
+// }
 // Program No. 5
 // 5. Write a ts program to check whether a number is even or odd.
-var num = prompt("Enter a number");
-var tempNum = parseInt(num) % 2;
-if (tempNum == 0) {
-    console.log("num is even");
-}
-else {
-    console.log("num is odd");
+// var num:any = prompt("Enter a number")
+// var tempNum:number = parseInt(num) % 2;
+// if (tempNum == 0) {
+//     console.log("num is even");
+// }
+// else {
+//     console.log("num is odd");
+// }
+// Program No. 06
+// 6. Write a ts program to check whether a year is leap year or not.
+var y = prompt("Enter your year");
+var year = parseInt(y);
+if (year % 4 == 0) {
+    console.log("your enters year is leap year");
 }
--- app.ts
+++ app.ts
@@ -2,83 +2,94 @@
 // 1. Write a ts program to find maximum between two numbers.
 // Enter Tow Numbers:
 
-var num1:number = 10;
-var num2:number = 20;
+// var num1:number = 10;
+// var num2:number = 20;
 
-if (num1 > num2) {
-    console.log("num1 is greater than num2");
-}
-else if (num1 < num2) {
-    console.log("num2 is greater than num1");
-}
-else{
-    console.log("both numbers are equal");
-}
+// if (num1 > num2) {
+//     console.log("num1 is greater than num2");
+// }
+// else if (num1 < num2) {
+//     console.log("num2 is greater than num1");
+// }
+// else{
+//     console.log("both numbers are equal");
+// }
 
 
 // Program No. 02
 // 2. Write a ts program to find maximum between Three numbers.
 // Enter Three Numbers:
 
-var num3:number = 10
-var num4:number = 20
-var num5:number = 30
+// var num3:number = 10
+// var num4:number = 20
+// var num5:number = 30
 
-if (num3>num4 && num3>num5) {
-    console.log("num3 is greater both num4 and num5");
-}
-else if (num4>num3 && num5>num5){
-    console.log("num4 is greater both num3 and num5");
-}
-else if (num5>num3 && num5>num2){
-    console.log("num5 is greater both num3 and num4");
-}
-else{
-    console.log("All three numbers are equal");
-}
+// if (num3>num4 && num3>num5) {
+//     console.log("num3 is greater both num4 and num5");
+// }
+// else if (num4>num3 && num5>num5){
+//     console.log("num4 is greater both num3 and num5");
+// }
+// else if (num5>num3 && num5>num2){
+//     console.log("num5 is greater both num3 and num4");
+// }
+// else{
+//     console.log("All three numbers are equal");
+// }
 
 // Program No. 03
 // 3. Write a ts program to check whether a number is negative, positive or zero.
 
-var num6:number = 1
+// var num6:number = 1
 
-if (num6 < 0) {
-    console.log("num6 is Negative");
-}
-else if (num6 > 0) {
-    console.log("num6 is Positive");
-}
-else{
-    console.log("num6 is zero");
-}
+// if (num6 < 0) {
+//     console.log("num6 is Negative");
+// }
+// else if (num6 > 0) {
+//     console.log("num6 is Positive");
+// }
+// else{
+//     console.log("num6 is zero");
+// }
 
 // Program Node. 04
 // 4. Write a ts program to check whether a number is divisible by 5 and 11 or not.
 
-var numSet = 110;
+// var numSet = 110;
 
-if (numSet % 5 == 0 && numSet % 11 == 0) {
-    console.log("the enter number is divisible by 5 and 11", numSet);
-}
-else if (numSet % 5 == 0) {
-    console.log("this number only divisible on 5");
-}
-else if (numSet % 11 == 0) {
-    console.log("this number only divisible on 11");
-}
-else {
-    console.log("this number is not divisibl on them");
-}
+// if (numSet % 5 == 0 && numSet % 11 == 0) {
+//     console.log("the enter number is divisible by 5 and 11", numSet);
+// }
+// else if (numSet % 5 == 0) {
+//     console.log("this number only divisible on 5");
+// }
+// else if (numSet % 11 == 0) {
+//     console.log("this number only divisible on 11");
+// }
+// else {
+//     console.log("this number is not divisibl on them");
+// }
 
 // Program No. 5
 // 5. Write a ts program to check whether a number is even or odd.
 
-var num:any = prompt("Enter a number")
-var tempNum:number = parseInt(num) % 2;
+// var num:any = prompt("Enter a number")
+// var tempNum:number = parseInt(num) % 2;
 
-if (tempNum == 0) {
-    console.log("num is even");
-}
-else {
-    console.log("num is odd");
+// if (tempNum == 0) {
+//     console.log("num is even");
+// }
+// else {
+//     console.log("num is odd");
+// }
+
+// Program No. 06
+// 6. Write a ts program to check whether a year is leap year or not.
+
+var y:any = prompt("Enter your year");
+var year:number = parseInt(y);
+
+if (year % 4 == 0) {
+    console.log("your enters year is leap year");
+    
 }
