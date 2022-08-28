9cb86329f515379fa0d9f1c77578ccb3debfbf94
dev0-arslan <arslanansarii947@gmail.com>
2022-08-28 05:02:16 -0700

    num2 is greater than num1

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var num2 = 20;
 if (num1 > num2) {
     console.log("num1 is greater than num2");
 }
-else if (num2 > num1) {
+else if (num1 < num2) {
     console.log("num2 is greater than num1");
 }
 else {
-    console.log("num1 is equal to num2");
+    console.log("both numbers are equal");
 }
